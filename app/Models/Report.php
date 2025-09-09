60fde3071be7900a9da29e89b8523d13584dafbf
bpikaar <bobpikaar@gmail.com>
2025-09-09 16:22:12 +0200

    FIX cast date_of_work (in Model) to datetime type so it becomes a Carbon instance

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'date_of_work' => 'datetime', // will be cast to Carbon
+    ];
 }
