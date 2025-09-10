15f4cbcecfa8444469a12c92d388cff11e39ca78
bpikaar <bobpikaar@gmail.com>
2025-09-10 13:38:48 +0200

    CHANGE logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ Vite::asset('resources/images/logo-infrascout.png') }}" alt="" class="block h-9 mr-3 w-auto fill-current/>
                     </a>
                 </div>
 
