0a6014f77c98ebe98f162010e037ad8921d4ee1b
bpikaar <bobpikaar@gmail.com>
2025-09-10 13:30:25 +0200

    ADD seeder for projects and user

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // Use Carbon to randomize. now() is called once per seeder run.
+        $created = now()->subDays(rand(1, 30));
+        $updated = (clone $created)->addDays(rand(1, 5));
+
+        return [
+            'number' => fake()->unique()->numberBetween(10000, 99999),
+            'name' => fake()->company(),
+            'created_at' => $created,
+            'updated_at' => $updated,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Bob Pikaar',
+            'email' => 'b.pikaar@hr.nl',
+            'password' => bcrypt('test1234')
         ]);
+
+        $this->call(ProjectSeeder::class);
     }
 }
