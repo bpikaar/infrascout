7ac4fe5a00d3b4260b868476022c53a0eb09e317
bpikaar <bobpikaar@gmail.com>
2025-09-19 12:27:14 +0200

    CHANGE project.index, smaller rows

--- resources/views/components/projects/project.blade.php
+++ resources/views/components/projects/project.blade.php
@@ -5,9 +5,9 @@
         <img src="{{ $project->thumbnail ? asset('images/projects/'.$project->thumbnail) : Vite::asset('resources/images/thumb-image.png') }}"
              alt="{{ $project->name }} thumbnail"
              class="h-full aspect-square rounded-l-xl rounded-r-none object-cover"
-             style="min-width: 64px; max-width: 128px;" />
+             style="min-width: 64px; max-width: 100px;" />
         <div class="flex-1 ml-4">
-            <h2 class="text-lg font-semibold group-hover:text-indigo-400 group-hover:dark:text-indigo-600">{{ $project->name }}</h2>
+            <h2 class="md:text-lg font-semibold group-hover:text-indigo-400 group-hover:dark:text-indigo-600">{{ $project->name }}</h2>
             <p class="text-sm text-gray-500">{{ $project->created_at->toFormattedDayDateString() }}</p>
         </div>
     </div>
